0132d922d1a381df6393769b4759fb16fb47c5d2
thomas.heuker <thomas.heuker@budderfly.com>
2023-06-06 12:07:03 -0400

    Switch to pinia-plugin-persistedstate

--- src/main/webapp/app/main.ts
+++ src/main/webapp/app/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { AppVue } from './common/primary/app';
 import router from './router/router';
 import { createPinia } from 'pinia';
-import piniaPersist from 'pinia-plugin-persist';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import './tailwind.css';
 
 // jhipster-needle-main-ts-import
@@ -10,7 +10,7 @@ import './tailwind.css';
 const app = createApp(AppVue);
 app.use(router);
 const pinia = createPinia();
-pinia.use(piniaPersist);
+pinia.use(piniaPluginPersistedstate);
 app.use(pinia);
 
 // jhipster-needle-main-ts-provider
